b3e5f893cfae05e861aefeede03c40280757868f
Shreya Agrawal <shreya.agrawal2121@gmail.com>
2021-10-12 23:21:07 +0530

    FSM Design modified

--- Traffic-Signal/traffic_signal.cpp
+++ Traffic-Signal/traffic_signal.cpp
@@ -2,10 +2,95 @@
 #include <bits/stdc++.h>
 using namespace std;
 vector<int> waiting_queue(8, 0);
-//Function to return number of cars waiting on a particular combination of paths
-int traffic_on_spot(pair<int, int> p)
+//FSM  class stores possible states of traffic lights as pair of ints and transitions as per input
+//Input is expressed as an integer in the range of 0 to 255 each bit indicating
+//presence or absence of traffic at that crossing.
+class FSM
 {
-    return (waiting_queue[p.first] + waiting_queue[p.second]);
+
+public:
+    vector<pair<int, int>> states;
+    int trans[13][256];
+    //To ensure optimality for any input a state is formed such that traffic exists on both crossings
+    //If it is not so then a state is found such that traffic is present at least in one of the crossings
+    //otherwise the state doesn't changes or goes to state 0 (when all lights are off).
+    FSM(vector<pair<int, int>> state)
+    {
+        bool possible2, possible1;
+
+        states = state;
+        for (int i = 0; i < 13; i++)
+            trans[i][0] = 0;
+        for (int i = 0; i < 13; i++)
+        {
+            for (int j = 1; j < 256; j++)
+            {
+                possible2 = possible1 = false;
+                for (int k = 1; k < 13; k++)
+                {
+                    if (i == k)
+                        continue;
+                    else
+                    {
+                        int p1 = states[k].first;
+                        int p2 = states[k].second;
+                        if ((j & (1 << p1)) && (j & (1 << p2)))
+                        {
+                            possible2 = true;
+                            trans[i][j] = k;
+                            break;
+                        }
+                    }
+                }
+                if (possible2 == false)
+                {
+                    for (int k = 1; k < 13; k++)
+                    {
+                        if (i == k)
+                            continue;
+                        else
+                        {
+                            int p1 = states[k].first;
+                            int p2 = states[k].second;
+                            if ((j & (1 << p1)) || (j & (1 << p2)))
+                            {
+                                possible1 = true;
+                                trans[i][j] = k;
+                                break;
+                            }
+                        }
+                    }
+                    if (possible1 == false)
+                    {
+                        if (i == 0)
+                            trans[i][j] = 0;
+                        else
+                        {
+                            int p1 = states[i].first;
+                            int p2 = states[i].second;
+                            if ((j & (1 << p1)) || (j & (1 << p2)))
+                                trans[i][j] = i;
+                            else
+                                trans[i][j] = 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+};
+//Function to convert the traffic input to a number in range of 0 to 255
+int get_input()
+{
+    int sum = 0;
+    for (int i = 0; i < 8; i++)
+    {
+        if (waiting_queue[i])
+        {
+            sum += ((1 << i));
+        }
+    }
+    return (sum);
 }
 void print_queue()
 {
@@ -40,63 +125,40 @@ void print_result(pair<int, int> p)
         }
     }
 }
-//Function to decide the status of traffic lights
-pair<int, int> set_status(pair<int, int> p)
-{
-    pair<int, int> status = {-2, -2};
-    if (waiting_queue[p.first] > 0)
-        status.first = p.first;
-    if (waiting_queue[p.second] > 0)
-        status.second = p.second;
-    return (status);
-}
+
 //Function to modify list of waiting cars
 void modify_queue(pair<int, int> p)
 {
     waiting_queue[p.second] = max(0, waiting_queue[p.second] - 1);
     waiting_queue[p.first] = max(0, waiting_queue[p.first] - 1);
 }
-//Function to print the overall status at each time step
-void generate_output(pair<int, int> p)
-{
-    pair<int, int> status;
-
-    status = set_status(p);
-    print_result(status);
-    cout << "Initial queue :";
-    print_queue();
-    modify_queue(p);
-    cout << "Final queue :";
-    print_queue();
-}
-//Function to check if traffic is there at any possible combination  of valid routes
-int check_availability(vector<pair<int, int>> states)
-{
-    int i;
-    pair<int, int> available = {-1, -1};
-
-    for (i = 0; i < 12; i++)
-    {
-        if (waiting_queue[states[i].first] > 0 && waiting_queue[states[i].second] > 0)
-        {
-            available = states[i];
-            break;
-        }
+//Function to get the actual status of lights at each time step
+pair<int,int> set_status(pair<int,int> p){
+    pair<int,int>s;
+    s.first = -2;
+    s.second = -2;
+    if(waiting_queue[p.first] > 0){
+        s.first = p.first;
     }
-    return (i);
+    if(waiting_queue[p.second] > 0){
+        s.second = p.second;
+    }
+    return(s);
 }
 int main()
 {
     vector<int> traffic_flow(8, 0);
-    int prev_state, next_state, t, i, j, time_step = 0,available;
-    //States is a vector of pairs  indicating state of lights in  a predefined sequential order
-    vector<pair<int, int>> states = {{0, 2}, {4, 6}, {1, 3}, {5, 7}, {0, 1}, {2, 3}, {4, 5}, {6, 7}, {0,7}, {1,4}, {2,5}, {3,6}};
+    int prev_state, next_state, t, i, j;
+    //States is a vector of pairs  pair of  valid transitions
+    vector<pair<int, int>> states = {{-1, -1}, {0, 2}, {4, 6}, {1, 3}, {5, 7}, {0, 1}, {2, 3}, {4, 5}, {6, 7}, {0, 7}, {1, 4}, {2, 5}, {3, 6}};
     prev_state = 0;
-    pair<int, int> p, new_p, status;
+    
     cin >> t;
-
+    FSM F(states);
+    int k = 1;
     while (true)
     {
+
         if (t > 0)
         {
             for (i = 0; i < 8; i++)
@@ -106,69 +168,25 @@ int main()
             }
             t--;
         }
+        int input = get_input();
+        next_state = F.trans[prev_state][input];
+        if (next_state != 0)
+        {
+            cout<<"Time step : "<<k<<"\n";
+            pair<int,int> s =set_status(states[next_state]);
 
-        for (j = 0; j < 8; j++)
-        {
-            if (waiting_queue[j] != 0)
-                break;
+            print_result(s);
+            cout<<"Initial queue :";
+            print_queue();
+            modify_queue(states[next_state]);
+            cout<<"Final queue :";
+            print_queue();
+            k++;
         }
-        if (j == 8 && t <= 0)
-            break;
-        p = states[prev_state];
-        new_p = states[prev_state + 1];
-        //Switch statement modifies the list of waiting cars ,prints output and generated the next state
-        switch (prev_state)
-        {
-        default:
-            if (traffic_on_spot(p) > 0)
-            {
-                ++time_step;
-                cout << "Time step " << time_step << ":\n";
-                generate_output(p);
-               
-            }
-            if (traffic_on_spot(p) > 0 && traffic_on_spot(new_p) <= 0)
-            {
-                next_state = prev_state;
-            }
-            else
-            {
-                available = check_availability(states);
-                if((waiting_queue[new_p.first] == 0 || waiting_queue[new_p.second] == 0) && available != 12)
-                {
-                    next_state = available;
-                }
-                else
-                    next_state = prev_state + 1;
-            }
+        else
             break;
 
-        case 11:
-            if (traffic_on_spot(p) > 0)
-            {
-                ++time_step;
-                cout << "Time step " << time_step << ":\n";
-                generate_output(p);
-                
-            }
-            if (traffic_on_spot(p) > 0 && traffic_on_spot(new_p) <= 0)
-            {
-                next_state = prev_state;
-            }
-            else
-            {
-                available = check_availability(states);
-                if((waiting_queue[new_p.first] == 0 || waiting_queue[new_p.second] == 0) && available != 12)
-                {
-                    next_state = available;
-                }
-                else
-                    next_state = 0;
-            }
-            break;
-        }
         prev_state = next_state;
     }
-
     return 0;
 }
