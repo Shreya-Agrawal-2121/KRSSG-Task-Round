39d88b8ea90b3ba5bdc2ef310cb53b9fc958fce3
Shreya Agrawal <shreya.agrawal2121@gmail.com>
2021-10-20 00:08:53 +0530

    Bonus task code modifed

--- Traffic-Signal/Client_bonus_task.cpp
+++ Traffic-Signal/Client_bonus_task.cpp
@@ -1,149 +1,214 @@
 #include <arpa/inet.h>
+
 #include <stdio.h>
+
 #include <string.h>
+
 #include <sys/socket.h>
+
 #include <unistd.h>
+
 #include <iostream>
+
 #include <bits/stdc++.h>
+
 using namespace std;
 
 vector<int> waiting_queue(8, 0);
+
 void print_queue()
+
 {
+
     for (int i = 0; i < 8; i++)
+
         cout << waiting_queue[i] << " ";
+
     cout << "\n";
 }
-//Function to print status of traffic lights
+
+// Function to print status of traffic lights
+
 void print_result(pair<int, int> p)
+
 {
+
     vector<char> lights{'A', 'B', 'C', 'D'};
+
     vector<string> msg = {"off", "go straight", "go right"};
+
     int i, status_first, status_second;
 
     for (i = 0; i < 4; i++)
+
     {
+
         cout << lights[i] << " ";
+
         if (i != p.first / 2 && i != p.second / 2)
+
         {
+
             cout << msg[0] << "\n";
         }
+
         else
+
         {
+
             if (2 * i == p.first || 2 * i == p.second)
+
                 cout << msg[1] << " ";
 
             if (2 * i + 1 == p.first || 2 * i + 1 == p.second)
+
             {
+
                 cout << msg[2] << " ";
             }
+
             cout << "\n";
         }
     }
 }
-//Function to decide the status of traffic lights
+
+// Function to decide the status of traffic lights
+
 pair<int, int> set_status(pair<int, int> p)
+
 {
+
     pair<int, int> status = {-2, -2};
+
     if (waiting_queue[p.first] > 0)
+
         status.first = p.first;
+
     if (waiting_queue[p.second] > 0)
+
         status.second = p.second;
+
     return (status);
 }
+
 void modify_queue(pair<int, int> p)
+
 {
+
     waiting_queue[p.second] = max(0, waiting_queue[p.second] - 1);
+
     waiting_queue[p.first] = max(0, waiting_queue[p.first] - 1);
 }
+
 void generate_output(pair<int, int> p)
+
 {
+
     pair<int, int> status;
 
     status = set_status(p);
+
     print_result(status);
+
     cout << "Initial queue :";
+
     print_queue();
+
     modify_queue(p);
+
     cout << "Final queue :";
+
     print_queue();
 }
 
 int main(int argc, char *argv[])
+
 {
+
     int sock;
+
     struct sockaddr_in server;
+
     pair<int, int> p;
 
+    vector<pair<int, int>> states = {{-1, -1}, {0, 2}, {4, 6}, {1, 3}, {5, 7}, {0, 1}, {2, 3}, {4, 5}, {6, 7}, {0, 7}, {1, 4}, {2, 5}, {3, 6}};
+
     vector<int> traffic_flow(8, 0);
+
     int t, i, j, k, step = 0, cnt;
+
     cout << "Enter number of time steps till which traffic will flow in:\n";
+
     cin >> t;
+
     int num;
+
     // Client is connected to the server
+
     sock = socket(AF_INET, SOCK_STREAM, 0);
+
     server.sin_addr.s_addr = inet_addr("127.0.0.1");
+
     server.sin_family = AF_INET;
+
     server.sin_port = htons(8880);
+
     connect(sock, (struct sockaddr *)&server, sizeof(server));
+
     send(sock, &t, sizeof(int), 0);
 
-    if (t > 0)
-    {
-        for (i = 0; i < 8; i++)
-            cin >> traffic_flow[i];
-
-        for (i = 0; i < 8; i++)
-            waiting_queue[i] += traffic_flow[i];
-        //At each step the data of traffic flow is sent to server
-        for (i = 0; i < 8; i++)
-        {
-            num = traffic_flow[i];
-            send(sock, &num, sizeof(int), 0);
-        }
-
-        t--;
-    }
+    int prev_state, next_state;
 
     while (true)
+
     {
-        /*The infinite loop breaks when all enteries in
-        the initial_queue are zero no other traffic will flow in*/
-        for (j = 0; j < 8; j++)
-        {
-            if (waiting_queue[j] != 0)
-                break;
-        }
-        if (j == 8 && t <= 0)
-            break;
-        //Server returns the valid routes to be taken for a particular step as positions in the array
-        int pos[2];
-        recv(sock, &pos, 2 * sizeof(int), 0);
-        p.first = pos[0];
-        p.second = pos[1];
-
-        ++step;
-
-        cout << "Time step " << step << ": \n";
-        generate_output(p);
 
         if (t > 0)
+
         {
+
             for (i = 0; i < 8; i++)
+
                 cin >> traffic_flow[i];
 
             for (i = 0; i < 8; i++)
+
                 waiting_queue[i] += traffic_flow[i];
 
+            // At each step the data of traffic flow is sent to server
+
             for (i = 0; i < 8; i++)
+
             {
+
                 num = traffic_flow[i];
+
                 send(sock, &num, sizeof(int), 0);
             }
-
-            t--;
         }
+
+        // Server returns the valid routes to be taken for a particular step as positions in the array
+
+        t--;
+
+        recv(sock, &next_state, sizeof(int), 0);
+
+        ++step;
+
+        if (next_state != 0)
+        {
+
+            cout << "Time Step :" << step << "\n";
+
+            generate_output(states[next_state]);
+        }
+
+        else
+
+            break;
     }
+
     close(sock);
+
     return 0;
 }
--- Traffic-Signal/Controller_bonus_task.cpp
+++ Traffic-Signal/Controller_bonus_task.cpp
@@ -1,149 +1,281 @@
 #include <arpa/inet.h>
+
 #include <stdio.h>
+
 #include <string.h>
+
 #include <sys/socket.h>
+
 #include <unistd.h>
+
 #include <iostream>
+
 #include <bits/stdc++.h>
+
 using namespace std;
 
 vector<int> waiting_queue(8, 0);
-//Function to return number of cars waiting on a particular combination of paths
-int traffic_on_spot(pair<int, int> p)
-{
-    return (waiting_queue[p.first] + waiting_queue[p.second]);
-}
-//Function to check if traffic is there at any possible combination  of valid routes
-int check_availability(vector<pair<int, int>> states)
-{
-    int i;
-    pair<int, int> available = {-1, -1};
 
-    for (i = 0; i < 12; i++)
+// FSM  class stores possible states of traffic lights as pair of ints and transitions as per input
+
+// Input is expressed as an integer in the range of 0 to 255 each bit indicating
+
+// presence or absence of traffic at that crossing.
+
+class FSM
+
+{
+
+public:
+    vector<pair<int, int>> states;
+
+    int trans[13][256];
+
+    // To ensure optimality for any input a state is formed such that traffic exists on both crossings
+
+    // If it is not so then a state is found such that traffic is present at least in one of the crossings
+
+    // otherwise the state doesn't changes or goes to state 0 (when all lights are off).
+
+    FSM(vector<pair<int, int>> state)
+
     {
-        if (waiting_queue[states[i].first] > 0 && waiting_queue[states[i].second] > 0)
+
+        bool possible2, possible1;
+
+        states = state;
+
+        for (int i = 0; i < 13; i++)
+
+            trans[i][0] = 0;
+
+        for (int i = 0; i < 13; i++)
+
         {
-            available = states[i];
-            break;
+
+            for (int j = 1; j < 256; j++)
+
+            {
+
+                possible2 = possible1 = false;
+
+                for (int k = 1; k < 13; k++)
+
+                {
+
+                    if (i == k)
+
+                        continue;
+
+                    else
+
+                    {
+
+                        int p1 = states[k].first;
+
+                        int p2 = states[k].second;
+
+                        if ((j & (1 << p1)) && (j & (1 << p2)))
+
+                        {
+
+                            possible2 = true;
+
+                            trans[i][j] = k;
+
+                            break;
+                        }
+                    }
+                }
+
+                if (possible2 == false)
+
+                {
+
+                    for (int k = 1; k < 13; k++)
+
+                    {
+
+                        if (i == k)
+
+                            continue;
+
+                        else
+
+                        {
+
+                            int p1 = states[k].first;
+
+                            int p2 = states[k].second;
+
+                            if ((j & (1 << p1)) || (j & (1 << p2)))
+
+                            {
+
+                                possible1 = true;
+
+                                trans[i][j] = k;
+
+                                break;
+                            }
+                        }
+                    }
+
+                    if (possible1 == false)
+
+                    {
+
+                        if (i == 0)
+
+                            trans[i][j] = 0;
+
+                        else
+
+                        {
+
+                            int p1 = states[i].first;
+
+                            int p2 = states[i].second;
+
+                            if ((j & (1 << p1)) || (j & (1 << p2)))
+
+                                trans[i][j] = i;
+
+                            else
+
+                                trans[i][j] = 0;
+                        }
+                    }
+                }
+            }
         }
     }
-    return (i);
-}
-void modify_queue(pair<int, int> p)
+};
+
+// Function to convert the traffic input to a number in range of 0 to 255
+
+int get_input()
+
 {
+
+    int sum = 0;
+
+    for (int i = 0; i < 8; i++)
+
+    {
+
+        if (waiting_queue[i])
+
+        {
+
+            sum += ((1 << i));
+        }
+    }
+
+    return (sum);
+}
+
+void modify_queue(pair<int, int> p)
+
+{
+
     waiting_queue[p.second] = max(0, waiting_queue[p.second] - 1);
+
     waiting_queue[p.first] = max(0, waiting_queue[p.first] - 1);
 }
+
 int main(int argc, char *argv[])
+
 {
-     //States is a vector of pairs  indicating state of lights in  a predefined sequential order
-    vector<pair<int, int>> states = {{0, 2}, {4, 6}, {1, 3}, {5, 7}, {0, 1}, {2, 3}, {4, 5}, {6, 7}, {0, 7}, {1, 4}, {2, 5}, {3, 6}};
+
+    // States is a vector of pairs  indicating state of lights in  a predefined sequential order
+
+    vector<pair<int, int>> states = {{-1, -1}, {0, 2}, {4, 6}, {1, 3}, {5, 7}, {0, 1}, {2, 3}, {4, 5}, {6, 7}, {0, 7}, {1, 4}, {2, 5}, {3, 6}};
+
     int num;
 
     int prev_state, next_state, t, i, j, time_step = 0, available;
+
     prev_state = 0;
+
     pair<int, int> p, new_p, status;
+
     //  Server socket is created and bound to a port
+
     //  Connection is established with a client and server is put in listening mode
+
     int socket_desc, client_sock, c, read_size;
+
     struct sockaddr_in server, client;
+
     socket_desc = socket(AF_INET, SOCK_STREAM, 0);
 
     server.sin_family = AF_INET;
+
     server.sin_addr.s_addr = INADDR_ANY;
+
     server.sin_port = htons(8880);
 
     bind(socket_desc, (struct sockaddr *)&server, sizeof(server));
+
     listen(socket_desc, 3);
+
     c = sizeof(struct sockaddr_in);
+
     client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t *)&c);
 
+    FSM F(states);
+
     recv(client_sock, &t, sizeof(int), 0);
+
     while (true)
+
     {
-        int pos[2];
+
         if (t > 0)
+
         {
 
             t--;
+
             for (i = 0; i < 8; i++)
+
             {
-                //Server receives information of traffic flow
-                //and modifies the queue of waiting cars accordingly.
+
+                // Server receives information of traffic flow
+
+                // and modifies the queue of waiting cars accordingly.
+
                 recv(client_sock, &num, sizeof(int), 0);
+
                 waiting_queue[i] += num;
             }
         }
-        p = states[prev_state];
-        new_p = states[prev_state + 1];
-         //Switch statement modifies the list of waiting cars ,prints output and generated the next state
-        switch (prev_state)
-        {
-        default:
-            if (traffic_on_spot(p) > 0)
-            {
-                pos[0] = p.first;
-                pos[1] = p.second;
-                send(client_sock, &pos, 2 * sizeof(int), 0);
-                modify_queue(p);
-                
-            }
-            if (traffic_on_spot(p) > 0 && traffic_on_spot(new_p) <= 0)
-            {
-                next_state = prev_state;
-            }
-            else
-            {
-                available = check_availability(states);
-                if ((waiting_queue[new_p.first] == 0 || waiting_queue[new_p.second] == 0) && available != 12)
-                {
-                    next_state = available;
-                }
-                else
-                    next_state = prev_state + 1;
-            }
-            break;
 
-        case 11:
-            if (traffic_on_spot(p) > 0)
-            {
-                //Server sends the valid status of traffic lights to client 
-                pos[0] = p.first;
-                pos[1] = p.second;
-                send(client_sock, &pos, 2 * sizeof(int), 0);
-                modify_queue(p);
-            }
-            if (traffic_on_spot(p) > 0 && traffic_on_spot(new_p) <= 0)
-            {
-                next_state = prev_state;
-            }
-            else
-            {
-                available = check_availability(states);
-                if ((waiting_queue[new_p.first] == 0 || waiting_queue[new_p.second] == 0) && available != 12)
-                {
-                    next_state = available;
-                }
-                else
-                    next_state = 0;
-            }
-            break;
-        }
+        int input = get_input();
+
+        next_state = F.trans[prev_state][input];
+
+        send(client_sock, &next_state, sizeof(int), 0);
+
+        if (next_state != 0)
+
+            modify_queue(states[next_state]);
+
         prev_state = next_state;
 
-        for (j = 0; j < 8; j++)
+        if (next_state == 0 && t <= 0)
+
         {
-            if (waiting_queue[j] != 0)
-                break;
-        }
-        if (j == 8 && t <= 0)
-        {
-            cout<<"Traffic cleared !! \n";
-                break;
+
+            cout << "Traffic cleared !! \n";
+
+            break;
         }
     }
 
     close(client_sock);
+
     close(socket_desc);
 
     return 0;
